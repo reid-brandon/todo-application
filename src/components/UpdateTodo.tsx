345184bb443e724f061ea327d694cb1b40c5b522
Brandon Reid <brandon.reid@pronavigator.ai>
2023-01-08 19:05:14 -0500

    Adding material ui, did some error checks, styling changes

--- src/components/UpdateTodo.tsx
+++ src/components/UpdateTodo.tsx
@@ -1,6 +1,10 @@
 import React, {useRef} from 'react';
 import {Todo} from '../models/todo.model';
 import './NewTodo.css';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
 
 interface UpdateTodoProps {
   todo: Todo;
@@ -34,14 +38,22 @@ const UpdateTodo: React.FC<UpdateTodoProps> = (props) => {
   }
 
   return (
-    <form onSubmit={todoSubmitHandler}>
-      <div className="form-control">
-        <label htmlFor="update-todo-text">Editing Todo</label>
-        <input type="text" id="update-todo-text" defaultValue={props.todo.text} ref={updateTextInputRef} />
-      </div>
-      <button type="submit">Update Todo</button>
-      <button onClick={todoCancelHandler}>Cancel</button>
-    </form>
+    <Card>
+      <form onSubmit={todoSubmitHandler}>
+        <div className="form-control-update">
+          <label htmlFor="update-todo-text">Editing the Task</label>
+          <input type="text" id="update-todo-text" className="input-padding-update-todo" defaultValue={props.todo.text} ref={updateTextInputRef} />
+        </div>
+        <Stack spacing={1} direction="row" justifyContent="right">
+          <Tooltip title="Update To Do">
+            <Button variant="contained" type="submit">Update</Button>
+          </Tooltip>
+          <Tooltip title="Cancel">
+            <Button variant="outlined" onClick={todoCancelHandler}>Cancel</Button>
+          </Tooltip>
+        </Stack>
+      </form>
+    </Card>
   );
 }
 
